e47f25b9b661fe673725ac60ea2b00a2fcfa8240
Marina Dvoretskova <smallestproblem@gmail.com>
2024-08-07 13:06:06 +0300

    Initial commit

---
+++ src/test/java/ru/yanmayak/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yanmayak.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
