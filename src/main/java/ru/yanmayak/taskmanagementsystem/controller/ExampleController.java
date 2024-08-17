4139bbf0af6c24e7ef331c08e6b3d3ad4ac7525f
Marina Dvoretskova <smallestproblem@gmail.com>
2024-08-17 14:55:17 +0300

    Fixed test controller

--- src/main/java/ru/yanmayak/taskmanagementsystem/controller/ExampleController.java
+++ src/main/java/ru/yanmayak/taskmanagementsystem/controller/ExampleController.java
@@ -3,6 +3,7 @@ package ru.yanmayak.taskmanagementsystem.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +16,17 @@ import ru.yanmayak.taskmanagementsystem.service.UserService;
 public class ExampleController {
     private final UserService userService;
 
-    @GetMapping("/api/private")
+    @GetMapping()
     @Operation(summary = "Доступно только для авторизованных пользователей")
     public String message() {
         return "hello, user";
     }
 
-    @GetMapping("/api/public")
-    @Operation(summary = "Доступно для всех")
-    public String message2() {
-        return "hello";
+    @GetMapping("/admin")
+    @Operation(summary = "Доступен только авторизованным пользователям с ролью ADMIN")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String exampleAdmin() {
+        return "Hello, admin!";
     }
 
     @GetMapping("/get-admin")
